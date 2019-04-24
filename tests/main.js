3046d9063c3bb70a2071e25cc90fd037fd68ae54
JDjedi <jcdiaz1201@gmail.com>
2019-04-24 17:20:14 -0500

    first commit

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 
+
 describe("short-lnk", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
@@ -16,5 +17,4 @@ describe("short-lnk", function () {
     it("server is not client", function () {
       assert.strictEqual(Meteor.isClient, false);
     });
-  }
 });
