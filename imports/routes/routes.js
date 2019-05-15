29689410bbd22f9d63523e58144fde99a7d408a1
JDjedi <jcdiaz1201@gmail.com>
2019-05-15 16:45:55 -0500

    Refactored routes for v4 compatibility

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,58 +1,30 @@
+
+
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { Router, Switch, Route, Redirect } from 'react-router-dom'; // react router syntax below v4! 
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
 import { createBrowserHistory } from 'history';
+import { Session } from 'meteor/session';
 
+import PrivateRoute from './privateRoute';
+import PublicRoute from './publicRoute';
 import Signup from '../ui/Signup';
 import Dashboard from '../ui/Dashboard';
-import Notfound from '../ui/NotFound';
+import NotFound from '../ui/NotFound';
 import Login from '../ui/Login';
 
-const history = createBrowserHistory();
-const unAuthenticatedPages = ['/signup', '/', '*', '/login'];
-const authenticatedPages = ['/dashboard']
+export const history = createBrowserHistory();
 
-
-const onEnterPublicPage = (Component) => {
-    if (Meteor.userId()) {
-        return <Redirect to="/dashboard" />
-    } else {
-        return <Component />
-    }
-}
-
-const onEnterPrivatePage = (Component) => {
-    if (!Meteor.userId()) {
-        return <Redirect to="/login" />
-    } else {
-        return <Component />
-    }
-}
-
-export const onAuthChange = (isAuthenticated) => {
-    const pathname = history.location.pathname;
-    const isUnauthenticatedPage = unAuthenticatedPages.includes(pathname);
-    const isAuthenticatedPage = authenticatedPages.includes(pathname);
-
-    if (isUnauthenticatedPage && isAuthenticated) {
-        history.push('/dashboard')
-    } else if (isAuthenticatedPage && !isAuthenticated) {
-        history.push('/login')
-    }
-}
-
-export const routes = ( 
-    <Router history={history}>
-        <Switch>
-            <Route exact path="/" component={Login} render={() => onEnterPublicPage(Login)} />
-            <Route exact path="/dashboard" component={Dashboard} render={() => onEnterPublicPage(Login)} />
-            <Route exact path="/signup" component={Signup} render={() => onEnterPublicPage(Signup)} />
-            <Route exact path="/login" component={Login} render={() =>  onEnterPrivatePage(Dashboard)} />
-            <Route exact path="*" component={Notfound} />
-        </Switch>
-    </Router>
+export const routes = (
+  <Router history={history}>
+    <Switch>
+      <PublicRoute path="/" component={Login} exact={true} />
+      <PublicRoute path="/signup" component={Signup} />
+      <PrivateRoute path="/dashboard" component={Dashboard} exact={true} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  </Router>
 );
-
 
 
 
